47edaba3bd25ad1f2a1053eb5c397bf03c98b744
Migeran Release <release@migeran.com>
2015-02-20 01:04:23 +0100

    LibGDX Migeran Backend Open Source Release

---
+++ migeran/src/main/java/com/badlogic/gdx/backends/lwjgl/LwjglApplication.java
@@ -0,0 +1,11 @@
+package com.badlogic.gdx.backends.lwjgl;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class LwjglApplication {
+
+	public LwjglApplication(ApplicationListener listener, String title, int width, int height) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
